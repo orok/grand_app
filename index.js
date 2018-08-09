4fbfb1879943872a3ce4c3e6ee05d1e663dbf5b3
Orokita Ukpong <orokitaukpong@OrokMBP.local>
2018-08-09 10:19:34 -0400

    Change files for final REST API

--- index.js
+++ index.js
@@ -4,95 +4,26 @@
  */
 
 // Dependencies
-const http = require('http');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
- // Configure the server to respond to all requests with a string
-let server = http.createServer(function(req,res){
+// Declare the app
+var app = {};
 
-  // Parse the url
-  let parsedUrl = url.parse(req.url, true);
+// Init function
+app.init = function(){
 
-  // Get the path
-  let path = parsedUrl.pathname;
-  let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // Start the server
+  server.init();
 
-  // Get the query string as an object
-  let queryStringObject = parsedUrl.query;
-
-  // Get the HTTP method
-  let method = req.method.toLowerCase();
-
-  //Get the headers as an object
-  let headers = req.headers;
-
-  // Get The Payload 
-  let decoder = new StringDecoder('utf-8');
-  let buffer = '';
-  req.on('data', function(data){
-    buffer += decoder.write(data);
-  });
-  req.on('end',function(){
-    buffer += decoder.end();
-
-    //Choose the request this handler should go to
-    let chosenHandler = typeof(router[trimmedPath]) != 'undefined'? router[trimmedPath]: handlers.notFound;
-
-    //construct data object to send to handler
-    let data = {
-        'trimmedPath': trimmedPath,
-        'queryStringObject': queryStringObject,
-        'method': method,
-        'headers': headers,
-        'payload': buffer
-    };
-
-    //Route the request to the handler specified in the router
-    chosenHandler(data, function(statusCode, payload){
-        // Use status code calledback by handler or default to 200 
-        statusCode =typeof(statusCode) == 'number'? statusCode: 200;
-
-        // Use payload called back by habdler or default to empty object 
-        payload = typeof(payload) == 'object'? payload : {};
-
-        //Convert the payload to a String 
-        let payloadString =JSON.stringify(payload);
-
-        //Return the response 
-        res.writeHead(statusCode);
-        res.end(payloadString);
-         // Log the request/response
-        console.log('Return this response :',statusCode, payloadString);
-    });
- 
-
-  });
-
-
-});
-
-// Start the server
-server.listen(3000,function(){
-  console.log('The server is up and running now');
-});
-
-//Define Handlers
-let handlers = {};
-
-//Sample Handler
-handlers.sample = function(data, callback){
-    // Callback HTTP status code anda payload
-    callback(406, {'name': 'sample handler'});
+  // Start the workers
+  workers.init();
 
 };
 
-// Not found handlers 
-handlers.notFound = function(data, callback){
-    callback(404);
-};
+// Self executing
+app.init();
 
-// Define a request router 
-let router ={
-    'sample': handlers.sample
-}
+
+// Export the app
+module.exports = app;
